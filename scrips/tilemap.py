99419756e2ce9efc6c3238d8de8b0e5c01272d69
Junior <eecfredes@gmail.com>
2023-10-01 20:12:10 -0300

    gravity

--- scrips/tilemap.py
+++ scrips/tilemap.py
@@ -8,12 +8,12 @@ class Tilemap:
         for i in range(10):
             self.tilemap[str(3 + i) + ";10"] = {
                 "type": "grass",
-                "variant": 1,
+                "variant": 5,
                 "pos": (3 + i, 10),
             }
             self.tilemap["10;" + str(5 + i)] = {
                 "type": "stone",
-                "variant": 1,
+                "variant": 7,
                 "pos": (10, 5 + i),
             }
 
